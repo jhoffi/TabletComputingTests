7124bb9f4b32b0f08ba36d2ab0c88a459718f1e5
Jonas <jon.hopeman.05@gmail.com>
2024-04-11 16:51:45 +0200

    Initial Commit

---
+++ TabletComputingTests/TabletComputingTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabletComputingTestsApp.swift
+//  TabletComputingTests
+//
+//  Created by Jonas Hoffmann on 11.04.24.
+//
+
+import SwiftUI
+
+@main
+struct TabletComputingTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
